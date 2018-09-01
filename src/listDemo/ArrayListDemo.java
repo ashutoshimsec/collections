62ec7c409ce7c60d25a558641f8b37ef7fd16ecc
Ashutosh Kumar <ashutoshimsec@gmail.com>
2018-09-01 17:34:07 +0530

    TreeSet added

--- src/listDemo/ArrayListDemo.java
+++ src/listDemo/ArrayListDemo.java
@@ -7,15 +7,17 @@ public class ArrayListDemo {
 	public static void main(String[] args) {
 
 		//first constructor without any paramerters
-		ArrayList<String> l = new ArrayList<>();
+		ArrayList l = new ArrayList();
 		
 		l.add("1");
 		l.add("5");
 		l.add("4");
-		l.add("5");
+		l.add(true);
 		l.add("9");
 		l.add("3");
-		l.add("32");
+		l.add(32);
+		l.add("welcome");
+		
 		
 		System.out.println("the ArrayList elemets are "+l);
 
--- src/set/TreeSetDemo.java
+++ src/set/TreeSetDemo.java
@@ -1,10 +1,38 @@
 package set;
 
+import java.util.LinkedHashSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class TreeSetDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		TreeSet lhs = new TreeSet() ;
+		
+	for(int i =120; i>100; i--){
+		lhs.add(i);
+	}
+	
+	//tresset will sort based on ascending order
+	
+	System.out.println("ceiling "+lhs.ceiling(10));	
+	
+	TreeSet hset = (TreeSet) lhs.headSet(110);
+	
+	TreeSet tset = (TreeSet) lhs.tailSet(110);
+	
+	TreeSet subSet =  (TreeSet) lhs.subSet(109, 118);
+	
+	System.out.println("headset is ==> "+hset);
+	System.out.println("tail set is ==> "+tset);
+	System.out.println("subset is ==>"+subSet );
+	
+	System.out.println("treeset comparator method is ==>"+lhs.comparator());  //by default it will return null
+	
+	
+	
 	}
 
 }
